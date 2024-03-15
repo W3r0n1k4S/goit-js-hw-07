32942dd58d5056e5176fef92b211fb1cec1f5c91
Weronika Skarbek <weronikaskarbek22@gmail.com>
2024-03-15 10:39:16 +0100

    Dodanie plikow zrodlowych zadania domowego 1

---
+++ js/task-02.js
@@ -0,0 +1,21 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ulIngredients = document.getElementById('ingredients');
+
+ingredients.forEach(function(ingredient) {
+
+    const li = document.createElement('li');
+
+    li.textContent = ingredient;
+
+    li.classList.add('item');
+
+    ulIngredients.appendChild(li);
+});
